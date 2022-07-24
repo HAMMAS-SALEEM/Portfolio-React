2512bedd911659fc77d028ca1921331151a49453
HAMMAS-SALEEM <hammassaleem376@gmail.com>
2022-07-25 00:23:06 +0500

    :art: Homepage is done

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import '../stylesheets/homepage.css';
 
 const Home = () => (
-  <h1>Home</h1>
+  <div className="homepage-component">
+    <h3 className="greeting">WELCOME TO MY WORLD!</h3>
+    <h1 className="hero-title">
+      Hi, I&apos;m Hammas
+      <br />
+      <span className="homepage-profession-title">.</span>
+      <br />
+      <span className="hero-title-two">
+        working remotely.
+      </span>
+    </h1>
+  </div>
 );
 
 export default Home;
