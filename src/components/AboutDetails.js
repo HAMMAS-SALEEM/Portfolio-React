bfaf4b8450927ddf2a33632ee83ec8ee2420ac4d
HAMMAS-SALEEM <hammassaleem376@gmail.com>
2022-07-26 04:32:48 +0500

    :art: Almost done with about me section

--- src/components/AboutDetails.js
+++ src/components/AboutDetails.js
@@ -1,91 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../stylesheets/aboutdetails.css';
+import { aboutDetails, aboutLinks } from './AboutSectionData';
 
 const AboutDetails = () => {
-  const links = [
-    { id: 1, link: 'Main Skills' },
-    { id: 2, link: 'Experience' },
-    { id: 3, link: 'Education & Certificate' },
-  ];
+  const [view, setView] = useState(0);
+  const handleLink = (index) => {
+    setView(index);
+  };
 
-  const details = [
-    [
-      {
-        title: 'Front-End Developer',
-        extraTitle: '',
-        description: 'JavaScript, React, Redux, HTML5, CSS3',
-      },
-      {
-        title: 'Back-End Developer',
-        extraTitle: '',
-        description: 'Ruby, Rails, PostgreSQL',
-      },
-      {
-        title: 'Professional',
-        extraTitle: '',
-        description: 'Remote Pair-Programming, Teamwork, Mentoring, Agile Methodologies',
-      },
-    ],
-    [
-      {
-        title: 'Technical Support Specialist (Project Reviewer)',
-        extraTitle: ' - Microverse',
-        description: 'June 2022 - Current',
-      },
-      {
-        title: 'Full Stack Web Developer',
-        extraTitle: ' - Self Employed',
-        description: 'November 2021 - Current',
-      },
-      {
-        title: 'SQL Developer',
-        extraTitle: ' - Synegic Professionals',
-        description: 'April 2021 - October 2021',
-      },
-    ],
-    [
-      {
-        title: 'Remote Full Stack Web Development Program, Full-Time',
-        extraTitle: ' - Microverse',
-        description: '2022',
-      },
-      {
-        title: 'BS(Hons.) Computer Science, Full-Time',
-        extraTitle: ' - GCUF',
-        description: '2021',
-      },
-    ],
-  ];
+  const handleActiveLink = (index) => (
+    index === view ? 'about-details-navlink active' : 'about-details-navlink'
+  );
   return (
     <>
       <ul className="about-details-navbar">
         {
-          links.map((link) => (
-            <li key={link.id} className="about-details-navlink">
-              {link.link}
-              <br />
-              <div className="about-navlink-border"> </div>
-            </li>
-          ))
-        }
+            aboutLinks.map((link, index) => (
+              <li key={link.id}>
+                <button onClick={(e) => handleLink(e.target.id)} id={index} className={handleActiveLink(index)} type="button">
+                  {link.link}
+                  <br />
+                  <div className="about-navlink-border"> </div>
+                </button>
+              </li>
+            ))
+          }
       </ul>
       <ul>
         {
-          details[0].map((detail) => (
-            <li key={new Date().getTime().toString()}>
-              <ul>
-                <li>
-                  {detail.title}
-                  <span>{detail.extraTitle}</span>
-                </li>
-                <li>{detail.description}</li>
-              </ul>
-            </li>
-          ))
-        }
+            aboutDetails[view].map((detail) => (
+              <li key={detail.id}>
+                <ul>
+                  <li className="about-me-skills-title">
+                    {detail.title}
+                    <span className="about-me-skills-extra-title">{detail.extraTitle}</span>
+                  </li>
+                  <li className="about-me-skills-description">{detail.description}</li>
+                </ul>
+              </li>
+            ))
+          }
       </ul>
     </>
   );
 };
-
 export default AboutDetails;
