22a871d03863ad4448d1f808b45ab48db52e226f
HAMMAS-SALEEM <hammassaleem376@gmail.com>
2022-07-25 13:21:13 +0500

    :construction: Working on about section

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,26 @@
 import React from 'react';
+// import Hammas from '../images/about/hammas.jpg';
+import '../stylesheets/about.css';
 
 const About = () => (
-  <h1>About</h1>
+  <div className="about-me-section">
+    <div className="about-me-picture">
+      {/* <img className="hammas-about-pic" src={Hammas} alt="hammas" /> */}
+      <div className="about-me-picture-overlay"> </div>
+    </div>
+    <div className="about-me-details">
+      <h2 className="about-me-title">About Me</h2>
+      <p className="about-me-par">
+        I&apos;m a software developer!
+        I can help you build a product,
+        feature or website Look through
+        some of my work and experience!
+        If you like what you see and have
+        a project you need coded, don&apos;t
+        hestiate to contact me.
+      </p>
+    </div>
+  </div>
 );
 
 export default About;
