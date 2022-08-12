89d80996c86c83a1d7433e0e6b585a785195fe48
HAMMAS-SALEEM <hammassaleem376@gmail.com>
2022-08-12 19:25:48 +0500

    :art: Updated width of about me section

--- src/components/AboutDetails.js
+++ src/components/AboutDetails.js
@@ -24,7 +24,7 @@ const AboutDetails = () => {
             ))
           }
       </ul>
-      <ul>
+      <ul className="about-details-container">
         {
             aboutDetails[view].map((detail) => (
               <li key={`detail-${detail.id}`}>
